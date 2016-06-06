b121006bc34b81a5c06c76188fcda6743852c77c
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 12:54:06 -0400

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -2,10 +2,10 @@ export USER="bsautner12"
 
 echo $(hostname -I | cut -d\  -f1) $(hostname) | tee -a /etc/hosts
 
-apt-get update -y
-apt-get upgrade -y
-apt-get dist-upgrade -y 
-apt-get autoremove -y 
+#apt-get update -y
+#apt-get upgrade -y
+#apt-get dist-upgrade -y 
+#apt-get autoremove -y 
 apt-get install ecryptfs-utils -y
 
 keyctl link @u @s
