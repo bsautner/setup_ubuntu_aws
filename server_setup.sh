5d150a7c7d6eb75ac00437740f5450519545197e
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 16:20:24 -0400

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -24,12 +24,7 @@ echo "ssh -i \"ubuntu-ws01-kp.pem\" ubuntu@$(hostname -I | cut -d\  -f1)"
 echo "cat /tmp/ubunut-ws01.pub >> ~/.ssh/authorized_keys"
  
 su - $USER
-#mkdir /home/$USER/.ssh
-#chown/home/$USER/.ssh $USER
-#chmod 700 /home/$USER/.ssh
-#touch /home/$USER/.ssh/authorized_keys
-#chmod 600 /home/$USER/.ssh/authorized_keys
-
+ 
 
 
 
