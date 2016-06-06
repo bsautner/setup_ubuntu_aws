3854da8f62b9aa9f7359120cab02bd37a575cd75
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 16:52:52 -0400

    Update me_setup.sh

--- me_setup.sh
+++ me_setup.sh
@@ -6,10 +6,13 @@ touch .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
 echo "Done setup paste the pub key into above"
 
+git config --global user.email $USER
+git config --global user.name "Benjamin Sautner"
+
+
 apt-get -y install pass
 pass init 
 pass git init
-git config --global user.email $USER
-git config --global user.name "Benjamin Sautner"
+
 
 
