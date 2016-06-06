6577c55cb5be86769ffa4bc87c3800c7d9ef655e
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 12:05:21 -0400

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -6,5 +6,12 @@ apt-get install ecryptfs-utils -y
 
 keyctl link @u @s
 adduser --encrypt-home --disabled-password bsautner
+keyctl unlink @u @s
+su - bsautner
+mkdir .ssh
+chmod 700 .ssh
+touch .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+
 
 
