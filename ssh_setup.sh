0b23e9b63ea37e98378815c7fae74566e9a353b1
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 16:17:28 -0400

    Update ssh_setup.sh

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,7 +1,11 @@
-export USER="bsautner"
+export USER="ben@sautner.me"
 
 mkdir .ssh
 chmod 700 .ssh
 touch .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
 echo "Done setup paste the pub key into above"
+
+apt-get -y install pass
+pass init 
+
