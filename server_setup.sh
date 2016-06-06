204be4fd48f78ce7912eac3a7c33cbe6dabcc1ca
Benjamin Sautner <bsautner@gmail.com>
2016-06-06 13:25:45 -0400

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -16,13 +16,20 @@ adduser $USER sudo
 bash -c 'echo "$USER ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)'
 
 #su - $USER
-mkdir /home/$USER/.ssh
-chmod 700 /home/$USER/.ssh
-touch /home/$USER/.ssh/authorized_keys
-chmod 600 /home/$USER/.ssh/authorized_keys
 
 echo "please logout and run the following on the client"
 echo "ssh-keygen -t rsa -b 4096 -f $USER@$(hostname -I | cut -d\  -f1)"
+echo "scp -i \"ubuntu-ws01-kp.pem\" ~/.ssh/ubuntu-ws01.pub  ubuntu@$(hostname -I | cut -d\  -f1):/tmp/ubunut-ws01.pub"
+echo "ssh -i \"ubuntu-ws01-kp.pem\" ubuntu@$(hostname -I | cut -d\  -f1)"
+echo "cat /tmp/ubunut-ws01.pub >> ~/.ssh/authorized_keys"
+ 
+
+#mkdir /home/$USER/.ssh
+#chown/home/$USER/.ssh $USER
+#chmod 700 /home/$USER/.ssh
+#touch /home/$USER/.ssh/authorized_keys
+#chmod 600 /home/$USER/.ssh/authorized_keys
+
 
 
 
