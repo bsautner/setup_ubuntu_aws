f8f512f7c1897ae61b03c672a4920ff20c293685
Benjamin Sautner <bsautner@gmail.com>
2016-06-07 09:22:15 -0400

    Update me_setup.sh

--- me_setup.sh
+++ me_setup.sh
@@ -7,13 +7,22 @@ chmod 600 /home/$USER/.ssh/authorized_keys
 chown ben -R /home/$USER/.ssh
 echo "Done setup paste the pub key into above"
 
+deluser ubuntu
+
 git config --global user.email $USER
 git config --global user.name "Benjamin Sautner"
 
-
+#Setup password manager
 apt-get -y install pass
 pass init $USER
 pass git init
+
+#Setup Proxy
+sudo apt-get install squid
+sudo cp /etc/squid3/squid.conf /etc/squid3/squid.conf.original
+sudo chmod a-w /etc/squid3/squid.conf.original
+sudo cp -vf ./squid.conf /etc/squid3/squid.conf 
+
 
 
 
