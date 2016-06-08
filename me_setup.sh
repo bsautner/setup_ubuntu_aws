6edc42ede8cdc09a847ede917a7d07eb117a25dd
Benjamin Sautner <bsautner@gmail.com>
2016-06-08 14:12:48 -0400

    Update me_setup.sh

--- me_setup.sh
+++ me_setup.sh
@@ -1,11 +1,5 @@
 export USER="ben"
 
-mkdir /home/$USER/.ssh
-chmod 700 home/$USER/.ssh.ssh
-touch /home/$USER/.ssh/authorized_keys
-chmod 600 /home/$USER/.ssh/authorized_keys
-chown ben -R /home/$USER/.ssh
-echo "Done setup paste the pub key into above"
 
 deluser ubuntu
 
