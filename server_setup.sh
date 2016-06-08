a21e8b5902e82c249e2ede4316fceb511329070e
Benjamin Sautner <bsautner@gmail.com>
2016-06-08 15:40:04 -0400

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -47,6 +47,23 @@ postconf -e 'smtpd_sasl_security_options = noanonymous'
 postconf -e 'broken_sasl_auth_clients = yes'
 postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
 postconf -e 'inet_interfaces = all'
+
+cp -v ./smtpd.conf /etc/postfix/sasl/smtpd.conf
+
+postconf -e 'smtp_tls_security_level = may'
+postconf -e 'smtpd_tls_security_level = may'
+postconf -e 'smtpd_tls_auth_only = no'
+postconf -e 'smtp_tls_note_starttls_offer = yes'
+postconf -e 'smtpd_tls_key_file = /etc/ssl/private/smtpd.key'
+postconf -e 'smtpd_tls_cert_file = /etc/ssl/certs/smtpd.crt'
+postconf -e 'smtpd_tls_CAfile = /etc/ssl/certs/cacert.pem'
+postconf -e 'smtpd_tls_loglevel = 1'
+postconf -e 'smtpd_tls_received_header = yes'
+postconf -e 'smtpd_tls_session_cache_timeout = 3600s'
+postconf -e 'tls_random_source = dev:/dev/urandom'
+postconf -e 'myhostname = central.sautner.me' 
+
+sudo apt-get -y install libsasl2-2 sasl2-bin libsasl2-modules
  
  reboot
 
