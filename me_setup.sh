38562f29aa6553a8bf8adee543791f211fb2a716
Benjamin Sautner <bsautner@gmail.com>
2016-07-26 15:25:00 -0400

    Update me_setup.sh

--- me_setup.sh
+++ me_setup.sh
@@ -5,18 +5,7 @@ deluser ubuntu
 
 git config --global user.email $USER
 git config --global user.name "Benjamin Sautner"
-
-#Setup password manager
-apt-get -y install pass
-pass init $USER
-pass git init
-
-#Setup Proxy
-sudo apt-get install squid
-sudo cp /etc/squid3/squid.conf /etc/squid3/squid.conf.original
-sudo chmod a-w /etc/squid3/squid.conf.original
-sudo cp -vf ./squid.conf /etc/squid3/squid.conf
-sudo service squid3 restart
+ 
 
 
 
